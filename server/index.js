e70c67b47fce90ba1669df4be019fbf4c58adef2
Mahmood787 <ali_khan87@live.com>
2023-11-28 06:31:38 +0500

    test

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const RegisterModel = require('./models/Register')
 const app = express()
 app.use(cors(
     {
-        origin: ["https://andrefa-mern-frontend.vercel.app/"],
+        origin: ["https://andrefa-mern-frontend.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
